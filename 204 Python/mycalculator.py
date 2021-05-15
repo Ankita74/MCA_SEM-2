a093e8bd557a9d8b599a8b73c17bfb07472508e1
Ankita Ahir <ankitaahit74@gmail.com>
2021-05-15 09:27:46 +0530

    second commit

---
+++ 204 Python/mycalculator.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def add(a,b):
+    return a + b
+def sub(a,b):
+    return a - b
+def mul(a , b):
+    return a * b
+def  div(a,b):
+    if(b>0):
+        return a/b
+    else:
+        return none
+
+
+# In[ ]:
+
+
+
+
